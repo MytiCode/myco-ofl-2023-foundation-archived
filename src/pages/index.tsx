13c91a7066776290c1c8b2180678632448124bb2
Ben Glassman <ben@myti.com>
2023-04-04 13:31:35 -0400

    Add tailwind and style the landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import mytiLogo from '../../public/myti-logo.png'
+import aliveImage from '../../public/its-alive.gif'
 
 export default function Home() {
   return (
@@ -11,10 +12,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main> 
+      <main className="p-12 flex flex-col items-center"> 
           <Image
             src={mytiLogo}
             alt="Myti"
+            priority
+            className="my-4"
+          />
+          <Image
+            src={aliveImage}
+            alt="It's Alive!!!!!"
             priority
           />
       </main>
