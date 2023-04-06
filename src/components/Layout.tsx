c90c5cb71eab3787d5992ac35898e59e4f6e7904
Ben Glassman <ben@myti.com>
2023-04-06 13:57:41 -0400

    Adopt a shared layout, move pickup sheets to its own page

---
+++ src/components/Layout.tsx
@@ -0,0 +1,51 @@
+import Head from "next/head"
+import Image from 'next/image'
+import mytiLogo from '../../public/myti-logo.png'
+import Link from 'next/link'
+
+interface LayoutProps {
+  title?: string
+  children: React.ReactNode
+}
+
+const navItems = [
+  { href: '/', label: 'Home' },
+  { href: '/pickup-sheets', label: 'Pickup Sheets' },
+];
+
+export default function Layout({ title, children }: LayoutProps) {
+  return (
+    <>
+      <Head>
+        <title>{title ? `${title} |` : ''} MyCo</title>
+        <meta name="description" content="Myti Core, It's alive!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="p-12 flex flex-col items-center"> 
+        <div className="flex items-center mb-4">
+          <Image
+            src={mytiLogo}
+            alt="Myti"
+            priority
+            className="pr-4"
+            height="75"
+          />
+          <p className="text-3xl">MyCo</p>
+        </div>
+        <nav className="flex">
+          {navItems.map(navItem => (
+            <Link
+              key={navItem.label}
+              href={navItem.href}
+              className="text-link mr-2"
+            >
+              {navItem.label}
+            </Link>
+          ))}
+        </nav>
+        {children}
+      </main>
+    </>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import mytiLogo from '../../public/myti-logo.png'
-import aliveImage from '../../public/its-alive.gif'
 import { OrderList } from '@/orders/OrderList'
+import Layout from '@/components/Layout'
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>MyCo</title>
-        <meta name="description" content="Myti Core, It's alive!" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="p-12 flex flex-col items-center"> 
-        <div className="flex items-center mb-4">
-          <Image
-            src={mytiLogo}
-            alt="Myti"
-            priority
-            className="pr-4"
-            height="75"
-          />
-          <p className="text-3xl">MyCo</p>
-        </div>
-          <OrderList />
-      </main>
-    </>
+    <Layout>
+      <OrderList />
+    </Layout>
   )
 }
