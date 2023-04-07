8fa9f6443920030f07d4d39cf04dc6f389699382
Ben Glassman <ben@myti.com>
2023-04-07 10:48:07 -0400

    WIP First pass at packing slips with e2e

--- e2e/pickup-sheets.spec.ts
+++ e2e/pickup-sheets.spec.ts
@@ -1,10 +1,42 @@
 import { test, expect } from "@playwright/test";
 
 test("Can navigate to pickup sheets", async ({ page }) => {
-  // TODO: Move to process.env.BASE_URL or something
-  await page.goto("http://localhost:3000/");
+  await page.goto("/");
 
   await page.getByTestId("nav-pickup-sheets").click();
 
   await expect(page).toHaveTitle(/Pickup Sheets/);
+});
+
+test.only("Can view packing slips", async ({ page }) => {
+  await page.goto("/packing-slips");
+
+  // Order data
+  const firstOrder = page.getByLabel("#1226");
+  await expect(firstOrder).toContainText("Jun 6, 2022, 11:16 AM");
+
+  // Shipping info
+  await expect(firstOrder).toContainText("Nobody Jones");
+  await expect(firstOrder).toContainText(
+    "9999 Excellent Drive Apt 1, Burlington, VT 05401"
+  );
+
+  // Line items
+  const lineItems = firstOrder.getByLabel("Line Items").getByRole("listitem");
+  await expect(lineItems).toHaveCount(2);
+
+  const lineItem = lineItems.filter({
+    hasText: "Auric Blends Perfume Oil - Moonlight",
+  });
+  await expect(lineItem).toBeVisible();
+  await expect(lineItem.getByLabel("Shop Name")).toHaveText("Homeport");
+  await expect(lineItem.getByRole("img")).toHaveAttribute(
+    "src",
+    "https://cdn.shopify.com/s/files/1/0578/9899/1785/products/PerfumeArmy_grande__06524.1649704087.386.513.jpg?v=1653412449"
+  );
+  await expect(lineItem).toContainText("SKU: 114658");
+  await expect(lineItem.getByLabel("Quantity Ordered")).toHaveText("1");
+  // await expect(lineItem.getByLabel("Quantity Fulfilled")).toHaveText("1");
+
+  // TODO: Partial fulfillment message
 });
--- src/pages/packing-slips.tsx
+++ src/pages/packing-slips.tsx
@@ -1,9 +1,38 @@
 import Layout from "@/components/Layout";
 
+import { LineItem, Order, Shop } from "../orders/model";
+import data from '@/data/pilot2-dummy-orders.json';
+import PackingSlip from "@/orders/PackingSlip";
+
+const shops = data.shops as unknown as Shop[];
+const shopsById = new Map<number, Shop>(
+  shops.map(s => [s.shopId, s])
+);
+const orders = (data.orders as unknown as Order[])
+  .map(order => ({
+    ...order,
+    lineItems: order.lineItems.map(li => ({
+      ...li,
+      shop: shopsById.get(li.shopId)! // should really check shop exists.
+    }))
+  }))
+
+export type LineItemViewModel = LineItem & {
+  shop: ShopViewModel
+}
+
+type ShopViewModel = Shop;
+
+export type OrderViewModel = Order & {
+  lineItems: LineItemViewModel[]
+}
+
 export default function PackingSlipsPage() {
   return (
     <Layout title="Packing Slips">
-      <p>I am the packing sheets!</p>
+      {orders.map(o => (
+        <PackingSlip key={o.orderId} order={o} />
+      ))}
     </Layout>
   );
 }
