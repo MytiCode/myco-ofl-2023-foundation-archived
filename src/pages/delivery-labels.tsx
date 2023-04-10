36e4a35dca04da3baa375d0074176e501c5c713b
Ben Glassman <ben@myti.com>
2023-04-10 16:04:21 -0400

    Implement hasty pass at styling for delivery labels
    We expect to iterate on this when we can test it

--- src/pages/delivery-labels.tsx
+++ src/pages/delivery-labels.tsx
@@ -1,18 +1,28 @@
 import Layout from "@/components/Layout";
 import { DeliveryLabel } from "@/delivery-labels/DeliveryLabel";
+import { chunk } from "lodash";
 
-import { LineItem, Order, Shop } from "../orders/model";
+import { Order } from "../orders/model";
 import data from '@/data/pilot2-dummy-orders.json';
 
 const orders = (data.orders as unknown as Order[]);
 
 export type OrderViewModel = Pick<Order, 'orderId' | 'orderNumber' | 'shippingAddress'>
 
+const labelsPerPage = 4;
+
 export default function DeliveryLabelsPage() {
+  const pages = chunk(orders, labelsPerPage);
+
+  // Split orders into chunks of 4 per page
   return (
     <Layout title="Delivery Labels">
-      {orders.map(order => (
-        <DeliveryLabel key={order.orderId} order={order} />
+      {pages.map((orders, index) => (
+        <div key={index} className="print:break-after-page print:p-10">
+          {orders.map(order => (
+            <DeliveryLabel key={order.orderId} order={order} />
+          ))}
+        </div>
       ))}
     </Layout>
   );
