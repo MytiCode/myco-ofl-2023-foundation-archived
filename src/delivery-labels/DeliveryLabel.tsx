d465d42359c75333182320e647ed557d453799ed
Ben Glassman <ben@myti.com>
2023-04-10 15:41:33 -0400

    Render delivery labels based on static file-based data

--- src/delivery-labels/DeliveryLabel.tsx
+++ src/delivery-labels/DeliveryLabel.tsx
@@ -1,16 +1,19 @@
+import { OrderViewModel } from "@/pages/delivery-labels";
+
 const labelIds = {
   label: (o: { orderId: number }) => `delivery-label-order-${o.orderId}`
 }
 
-export function DeliveryLabel() {
+export function DeliveryLabel({ order }: { order: OrderViewModel }) {
+  const { shippingAddress: address } = order;
   return (
-      <div aria-labelledby={labelIds.label({ orderId: 1 })}>
-        <h2 id={labelIds.label({ orderId: 1 })}>#1226-2</h2>
+      <div aria-labelledby={labelIds.label(order)}>
+        <h2 id={labelIds.label(order)}>{order.orderNumber}</h2>
         <img src="something.jpg" alt="Myti: Local is Mighty" />
         <p>
-          Nobody Jones<br />
-          9999 Excellent Drive, Apt 1,<br />
-          Burlington, VT 05401
+          {address.firstName} {address.lastName}<br />
+          {address.address1}{address.address2 ? ', ' + address.address2 : ''},<br />
+          {address.city}, {address.stateCode} {address.zip}
         </p>
         <p>Delivered By: </p>
       </div>
--- src/pages/delivery-labels.tsx
+++ src/pages/delivery-labels.tsx
@@ -1,10 +1,19 @@
 import Layout from "@/components/Layout";
 import { DeliveryLabel } from "@/delivery-labels/DeliveryLabel";
 
+import { LineItem, Order, Shop } from "../orders/model";
+import data from '@/data/pilot2-dummy-orders.json';
+
+const orders = (data.orders as unknown as Order[]);
+
+export type OrderViewModel = Pick<Order, 'orderId' | 'orderNumber' | 'shippingAddress'>
+
 export default function DeliveryLabelsPage() {
   return (
     <Layout title="Delivery Labels">
-      <DeliveryLabel />
+      {orders.map(order => (
+        <DeliveryLabel key={order.orderId} order={order} />
+      ))}
     </Layout>
   );
 }
