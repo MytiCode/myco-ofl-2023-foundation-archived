40167827d35ca30ab088a77ba2e5ac032ac98e97
Ben Glassman <ben@myti.com>
2023-04-10 21:36:36 -0400

    Write order tracking sheet with dummy data with e2e

--- e2e/order-tracking-sheets.spec.ts
+++ e2e/order-tracking-sheets.spec.ts
@@ -1,5 +1,7 @@
 import { expect } from "@playwright/test";
 import { test } from "./util";
+import * as XLSX from "xlsx";
+import { OrderTrackingSheetConfig } from "../src/pages/order-tracking-sheets";
 
 test("Can navigate to order tracking sheets", async ({
   page,
@@ -12,4 +14,54 @@ test("Can navigate to order tracking sheets", async ({
   await expect(orderTrackingSheetsPage.page).toHaveTitle(
     /Order Tracking Sheets/
   );
+});
+
+test("Can download order tracking sheet", async ({
+  page,
+  orderTrackingSheetsPage,
+}) => {
+  const downloadPromise = page.waitForEvent("download");
+
+  await orderTrackingSheetsPage.goto();
+
+  const download = await downloadPromise;
+  expect(download.suggestedFilename()).toEqual("test.xlsx");
+
+  // TODO: What path to use that works for local and CI?
+  // Delete the path afterwards or just put it somewhere tmp?
+  // TODO: Should we use createReadStream rather than saving to disk and then reading?
+  const path = `something-${Date.now()}.xlsx`;
+  await download.saveAs(path);
+
+  const book = XLSX.readFile(path);
+
+  const config = new OrderTrackingSheetConfig();
+  const ordersSheetLabel = "Orders";
+  const orders: any[] = XLSX.utils
+    .sheet_to_json(book.Sheets[ordersSheetLabel], {
+      header: config.forSheet(ordersSheetLabel).columnNames,
+    })
+    .slice(1);
+
+  expect(orders.length).toBeGreaterThan(0);
+  const order = orders[0];
+  expect(order).toBeDefined();
+  expect(order.orderNumber).toEqual("#1226-2");
+  expect(order.shopName).toEqual("Homeport");
+  expect(order.fulfillmentStatus).toEqual("unfulfilled");
+
+  const lineItemsSheetLabel = "Line Items";
+  const lineItems: any[] = XLSX.utils
+    .sheet_to_json(book.Sheets[lineItemsSheetLabel], {
+      header: config.forSheet(lineItemsSheetLabel).columnNames,
+    })
+    .slice(1);
+  expect(lineItems.length).toBeGreaterThan(0);
+  const lineItem = lineItems[0];
+  expect(lineItem).toBeDefined();
+  expect(lineItem.orderNumber).toEqual("#1226-2");
+  expect(lineItem.title).toEqual("Auric Blends Perfume Oil - Moonlight");
+  expect(lineItem.qty).toEqual("1");
+  expect(lineItem.qtyFulfilled).toEqual("1");
+  expect(lineItem.lineItemId).toEqual("11352135467177");
 });
--- src/pages/order-tracking-sheets.tsx
+++ src/pages/order-tracking-sheets.tsx
@@ -1,6 +1,106 @@
 import Layout from "@/components/Layout";
+import { useEffect } from "react";
+import * as XLSX from "xlsx";
+
+type OrderTrackingSheetLabel = "Orders" | "Line Items";
+
+export class OrderTrackingSheetConfig {
+
+  ORDER_COLUMNS = [
+    { headerLabel: "Order #", name: "orderNumber" },
+    { headerLabel: "Shop Name ", name: "shopName" },
+    { headerLabel: "Fulfillment Status", name: "fulfillmentStatus" },
+    { headerLabel: "Picked Up ", name: "pickedUp" },
+    { headerLabel: "At Depot ", name: "atDepot" },
+    { headerLabel: "QA ", name: "qa" },
+    { headerLabel: "In Delivery Route ", name: "inDeliveryRoute" },
+    { headerLabel: "Ready for Delivery ", name: "readyForDelivery" },
+    { headerLabel: "Out for Delivery ", name: "outForDelivery" },
+    { headerLabel: "Delivered ", name: "delivered" },
+    { headerLabel: "Action Needed ", name: "actionNeeded" },
+    { headerLabel: "Notes ", name: "notes" },
+  ];
+
+  LINE_ITEM_COLUMNS = [
+    { headerLabel: "Order #", name: "orderNumber" },
+    { headerLabel: "Item Name", name: "title" },
+    { headerLabel: "QTY Ordered", name: "qty" },
+    { headerLabel: "QTY Fulfilled", name: "qtyFulfilled" },
+    { headerLabel: "Line Item ID", name: "lineItemId" },
+    { headerLabel: "Action Needed", name: "actionNeeded" },
+    { headerLabel: "Notes", name: "notes" },
+  ];
+
+  get sheets(): Array<{ label: OrderTrackingSheetLabel, headers: string[], columnNames: string[] }> {
+    return [
+      { label: 'Orders', headers: this.ORDER_COLUMNS.map(c => c.headerLabel), columnNames: this.ORDER_COLUMNS.map(c => c.name) },
+      { label: 'Line Items', headers: this.LINE_ITEM_COLUMNS.map(c => c.headerLabel), columnNames: this.LINE_ITEM_COLUMNS.map(c => c.name) }
+    ];
+  }
+
+  forSheet(label: string) {
+    const config = this.sheets.find(c => c.label === label);
+    if (!config) {
+      throw new Error(`Could not find config for sheet with label "${label}"`);
+    }
+
+    return config;
+  }
+
+}
+class OrderTrackingSheetWriter {
+
+  constructor(private config: OrderTrackingSheetConfig = new OrderTrackingSheetConfig) {}
+
+  // TODO: Better types
+  write({ orders, lineItems, filename }: { orders: any[], lineItems: any[], filename: string }) {
+    const workbook = XLSX.utils.book_new();
+
+    const rowsByLabel = new Map<OrderTrackingSheetLabel, any[]>([
+      ["Orders", orders],
+      ["Line Items", lineItems]
+    ]);
+    this.config.sheets.forEach(({ label, headers }) => {
+      const rows = rowsByLabel.get(label);
+      if (!rows) throw new Error(`Could not find rows for ${label}`);
+      const sheet = this.buildSheet({ rows, headers });
+      XLSX.utils.book_append_sheet(workbook, sheet, label);
+    });
+
+    XLSX.writeFile(workbook, filename);
+  }
+  
+  private buildSheet({ rows, headers }: { rows: any[], headers: string[] }) {
+    rows.forEach(row => {
+      headers.forEach((_header, index) => {
+        if (!(index in row)) {
+          row[index] = "";
+        }
+      });
+      if (row.length !== headers.length) {
+        throw new Error(`row must be same length as headers. Expected ${headers.length} cells but got ${row.length}`);
+      }
+    });
+
+    return XLSX.utils.aoa_to_sheet([
+      headers,
+      ...rows
+    ]);
+  }
+}
 
 export default function OrderTrackingSheetsPage() {
+  useEffect(() => {
+    new OrderTrackingSheetWriter().write({
+      filename: 'test.xlsx',
+      orders: [
+        ["#1226-2", "Homeport", "unfulfilled"],
+      ],
+      lineItems: [
+        ["#1226-2", "Auric Blends Perfume Oil - Moonlight", "1", "1", "11352135467177"],
+      ]
+    });
+  }, []);
   return (
     <Layout title="Order Tracking Sheets">
       <p>I am the sheet!</p>
