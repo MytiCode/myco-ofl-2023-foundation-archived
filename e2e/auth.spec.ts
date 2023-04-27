d2ded06ff95affa25429ce4bb6966567bf63a7b6
Ben Glassman <ben@myti.com>
2023-04-27 14:15:44 -0400

    Implement invalid/expired token tests

--- e2e/auth.spec.ts
+++ e2e/auth.spec.ts
@@ -1,16 +1,44 @@
 import { expect } from "@playwright/test";
 import { test } from "./util";
 
+const ACCESS_TOKEN_SESSION_KEY = "myti-auth";
+
 test("Can login", async ({ page, tokenSigner }) => {
   const userId = `trevor-testeroni`;
   const token = tokenSigner.sign({ userId }).unwrap();
 
   await page.goto("/");
 
-  await page.getByLabel("WHO GOES THERE!").fill(token);
+  await page.getByLabel("Token").fill(token);
   await page.getByRole("button", { name: "Login" }).click();
 
   await expect(page.getByText(userId)).toBeVisible();
 });
 
-test("If I have an expired token, I'm asked to login", async ({ page }) => {});
+const expiredAccessToken =
+  "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InRlc3QtbXljby1hdXRoIn0.eyJzdWIiOiJiZW4iLCJpYXQiOjE2Nzk4NDQ1NDguODA5fQ.KKYf0DzgcuX5i1Q7MKAt9AV_MrldXYkC-mFLMhYaXNFsft4FzgfXMDs0ahmr3EF4ijPulPrnl8VnRd-aP_HAmOjs4ol905sTj3BChwA1tHnR4LMSS2JSX3PHadMPpzAaa5GqxiXG54711qNLKaIihy7Cf-6-3lL3Uk9EdcwlIkGSr_ib5QJj6ZtIQx6HcgQja1rTfTpJbkl5hgUGeyJRN_ZIuyz_xlX-QeL7URBjiUzPQkboAL7G7Oq3jVZHv4_nXzO5bTIG6ePcKAWeBtSsiufXywxwPVL0_nxecFXaz1AaDQEILj_8azEGyLLbXPCoZ8YPfZBeUR7_SindfFa2qd8fMiV82Je7LhrGT8u-FHuZ60iPXzGv9zyKdnRQMzIO-XLrMbXCO6kQ8eTuRILxcwfr_YX4MYf1wQq4ph7E01BP38CuO7JP0bccHzYGBxTwO2d3Cs7lRDVqBbrY5T0t2dgJrVz-TU5rpSB4kCNF5JnuYoQUXp_szAJ23lJ89eVCwXM-ILR3u4axmOrm7MqJ7IkLtXq2EL3m3fX_2Y7VPpoq9HuOVSY6KbqI6-gKaUO9zKZO1F7uBjR-x62yHJC6lqxhC6xBzKqZ8Tt4pjfHG6Goj1h4yOoBsQI9rI-tdWyyfVKKR_nhHtG01IvpyGzq0g-dZsWt1EaUwf6I-HwvF14";
+
+test("If I provide an expired token, I'm asked to login", async ({ page }) => {
+  await page.goto("/");
+  await page.getByLabel("Token").fill(expiredAccessToken);
+  await page.getByRole("button", { name: "Login" }).click();
+  await page.getByText("Invalid token");
+});
+
+test("If I have a remembered token that is not expired, I'm asked to login", async ({
+  page,
+}) => {
+  await page.goto("/?__sandbox");
+  await page.evaluate(
+    ({ key, token }) => {
+      window.localStorage.setItem(key, token);
+    },
+    {
+      key: ACCESS_TOKEN_SESSION_KEY,
+      token: expiredAccessToken,
+    }
+  );
+
+  await page.goto("/");
+  await page.getByText("Invalid token");
+});
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
+import ':styles/globals.css';
 import { MycoClient } from ':api/client';
-import ':styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ReactElement, createContext, useEffect, useState } from 'react'
 import jwt from 'jsonwebtoken';
@@ -7,7 +7,7 @@ import { z } from 'zod';
 import shallNotPassImage from '../../public/shall-not-pass.png';
 import Image from 'next/image';
 
-function Login({ onLogin }: { onLogin: (token: string) => void }) {
+function Login({ onLogin, error }: { onLogin: (token: string) => void, error?: string }) {
   return (
     <form
       className="p-2 w-1/2 mx-auto"
@@ -24,16 +24,19 @@ function Login({ onLogin }: { onLogin: (token: string) => void }) {
       }}
     >
       <Image src={shallNotPassImage} alt="YOU SHALL NOT PASS" className="mb-4" />
-      <label className="text-2xl font-bold">
-        YOU SHALL NOT PASS!<br />
-        <textarea placeholder="What is your token?" name="token" className="border-slate-500 border-2 w-full p-4"></textarea>
+      <label className="text-2xl font-bold" htmlFor="token">
+        YOU SHALL NOT PASS!
       </label>
+      {error &&
+        <p className="text-lg text-red-600 font-bold">Invalid token, please try again or ask for a fresh one, this one my be expired.</p>
+      }
+      <textarea id="token" aria-label="Token" placeholder="What is your token?" name="token" className="border-slate-500 border-2 w-full p-4"></textarea>
       <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Login</button>
     </form>
   );
 }
 
-const ACCESS_TOKEN_SESSION_KEY = 'myti-auth';
+export const ACCESS_TOKEN_SESSION_KEY = 'myti-auth';
 
 type User = { userId: string };
 /**
@@ -50,13 +53,23 @@ type User = { userId: string };
 export const APIContext = createContext<MycoClient>(null as unknown as MycoClient);
 export const UserContext = createContext<User>(null as unknown as User);
 
+type AppState = 'sandbox' | 'verify-token' | 'no-token' | 'invalid-token' | 'booted';
+
 function App({ children }: { children: ReactElement}) {
-  const [appState, setAppState] = useState<'verify-token' | 'no-token' | 'booted'>('verify-token');
+  const [appState, setAppState] = useState<AppState>('verify-token');
   const [apiClient, setAPIClient] = useState<MycoClient>();
   const [user, setUser] = useState<User>();
 
   useEffect(() => {
     (async () => {
+      // If the app is in sandbox mode, don't render anything
+      // This is a hook for tests to be able to load the app and, for example, put a token in local storage
+      // without executing any app code
+      if (new URLSearchParams(window.location.search).has('__sandbox')) {
+        setAppState('sandbox');
+        return;
+      }
+
       switch (appState) {
         case 'verify-token':
           const token = localStorage.getItem(ACCESS_TOKEN_SESSION_KEY);
@@ -70,9 +83,8 @@ function App({ children }: { children: ReactElement}) {
             const result = await api.getEntry();
             if (result.err) {
               localStorage.removeItem(ACCESS_TOKEN_SESSION_KEY);
-              window.location.reload();
+              setAppState('invalid-token');
             } else {
-              console.log(jwt.decode(token));
               const payload = z.object({
                 sub: z.string()
               }).parse(jwt.decode(token));
@@ -87,14 +99,21 @@ function App({ children }: { children: ReactElement}) {
     })();
   }, [appState]);
 
-  if (appState === 'no-token') {
+  if (appState === 'sandbox') {
+    return null;
+  }
+
+  if (appState === 'no-token' || appState === 'invalid-token') {
     return (
-      <Login
-        onLogin={async (token) => {
-          localStorage.setItem(ACCESS_TOKEN_SESSION_KEY, token);
-          setAppState('verify-token');
-        }}
-      />
+      <div>
+        <Login
+          error="Invalid token, please try again or ask for a fresh one, this one my be expired."
+          onLogin={async (token) => {
+            localStorage.setItem(ACCESS_TOKEN_SESSION_KEY, token);
+            setAppState('verify-token');
+          }}
+        />
+      </div>
     );
   }
 
