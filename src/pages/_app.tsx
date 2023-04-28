1b760564382d3f89041452b573287d78572f7bfa
Ben Glassman <ben@myti.com>
2023-04-27 21:34:35 -0400

    Gussy up the login page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ import Image from 'next/image';
 function Login({ onLogin, error }: { onLogin: (token: string) => void, error?: string }) {
   return (
     <form
-      className="p-2 w-1/2 mx-auto"
+      className="p-2 pt-6 w-1/2 mx-auto flex flex-col "
       onSubmit={e => {
         e.preventDefault();
 
@@ -23,15 +23,15 @@ function Login({ onLogin, error }: { onLogin: (token: string) => void, error?: s
         }
       }}
     >
-      <Image src={shallNotPassImage} alt="YOU SHALL NOT PASS" className="mb-4" />
-      <label className="text-2xl font-bold" htmlFor="token">
+      <Image src={shallNotPassImage} alt="YOU SHALL NOT PASS" className="mb-4 rounded-lg" />
+      <label className="text-4xl mb-6 block text-center font-bold text-white -mt-16" htmlFor="token">
         YOU SHALL NOT PASS!
       </label>
       {error &&
         <p className="text-lg text-red-600 font-bold">Invalid token, please try again or ask for a fresh one, this one my be expired.</p>
       }
-      <textarea id="token" aria-label="Token" placeholder="What is your token?" name="token" className="border-slate-500 border-2 w-full p-4"></textarea>
-      <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Login</button>
+      <textarea id="token" aria-label="Token" placeholder="What is the secret password?" name="token" className="border-slate-200 border-2 w-full p-4 mb-2 text-2xl rounded-md"></textarea>
+      <button type="submit" className="bg-teal-800 hover:bg-teal-700 text-lg text-white font-bold py-2 px-6 border rounded-md">Login</button>
     </form>
   );
 }
@@ -107,7 +107,7 @@ function App({ children }: { children: ReactElement}) {
     return (
       <div>
         <Login
-          error="Invalid token, please try again or ask for a fresh one, this one my be expired."
+          error={appState === 'invalid-token' ? "Invalid token, please try again or ask for a fresh one, this one my be expired." : undefined}
           onLogin={async (token) => {
             localStorage.setItem(ACCESS_TOKEN_SESSION_KEY, token);
             setAppState('verify-token');
