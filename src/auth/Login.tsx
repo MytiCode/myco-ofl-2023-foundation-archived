142dda40ddd931b4f9c4ff421022ff44eedddc44
Ben Glassman <ben@myti.com>
2023-05-01 20:09:30 -0400

    Move Login component to new auth module

---
+++ src/auth/Login.tsx
@@ -0,0 +1,31 @@
+import shallNotPassImage from '../../public/shall-not-pass.png';
+import Image from 'next/image';
+
+export function Login({ onLogin, error }: { onLogin: (token: string) => void, error?: string }) {
+  return (
+    <form
+      className="p-2 pt-6 w-1/2 mx-auto flex flex-col "
+      onSubmit={e => {
+        e.preventDefault();
+
+        const token = e.currentTarget.token.value;
+        if (token) {
+          onLogin(token);
+        } else {
+          // TODO(benglass): its required dude
+          console.log('naw dude, its required');
+        }
+      }}
+    >
+      <Image src={shallNotPassImage} alt="YOU SHALL NOT PASS" className="mb-4 rounded-lg" />
+      <label className="text-4xl mb-6 block text-center font-bold text-white -mt-16" htmlFor="token">
+        YOU SHALL NOT PASS!
+      </label>
+      {error &&
+        <p className="text-xl text-red-700 mb-2 font-bold">Invalid token, please try again (copy/paste error?) or ask for a fresh one, this one my be expired.</p>
+      }
+      <textarea id="token" aria-label="Token" placeholder="What is the secret password?" name="token" className="border-slate-200 border-2 w-full p-4 mb-2 text-2xl rounded-md"></textarea>
+      <button type="submit" className="bg-teal-800 hover:bg-teal-700 text-lg text-white font-bold py-2 px-6 border rounded-md">Login</button>
+    </form>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,41 +4,11 @@ import type { AppProps } from 'next/app'
 import { ReactElement, createContext, useEffect, useState } from 'react'
 import jwt from 'jsonwebtoken';
 import { z } from 'zod';
-import shallNotPassImage from '../../public/shall-not-pass.png';
-import Image from 'next/image';
-
-function Login({ onLogin, error }: { onLogin: (token: string) => void, error?: string }) {
-  return (
-    <form
-      className="p-2 pt-6 w-1/2 mx-auto flex flex-col "
-      onSubmit={e => {
-        e.preventDefault();
-
-        const token = e.currentTarget.token.value;
-        if (token) {
-          onLogin(token);
-        } else {
-          // TODO(benglass): its required dude
-          console.log('naw dude, its required');
-        }
-      }}
-    >
-      <Image src={shallNotPassImage} alt="YOU SHALL NOT PASS" className="mb-4 rounded-lg" />
-      <label className="text-4xl mb-6 block text-center font-bold text-white -mt-16" htmlFor="token">
-        YOU SHALL NOT PASS!
-      </label>
-      {error &&
-        <p className="text-xl text-red-700 mb-2 font-bold">Invalid token, please try again (copy/paste error?) or ask for a fresh one, this one my be expired.</p>
-      }
-      <textarea id="token" aria-label="Token" placeholder="What is the secret password?" name="token" className="border-slate-200 border-2 w-full p-4 mb-2 text-2xl rounded-md"></textarea>
-      <button type="submit" className="bg-teal-800 hover:bg-teal-700 text-lg text-white font-bold py-2 px-6 border rounded-md">Login</button>
-    </form>
-  );
-}
+import { User } from ':auth';
+import { Login } from ':auth/Login';
 
 export const ACCESS_TOKEN_SESSION_KEY = 'myti-auth';
 
-type User = { userId: string };
 /**
  * TODO(benglass) Figure out how to make dynamic values in a context non-option
  * Here we know for the rest of the app if they useContext(APIContext) they are guaranteed to get it
@@ -131,19 +101,6 @@ function App({ children }: { children: ReactElement}) {
 }
 
 export default function AnonymousApp({ Component, pageProps }: AppProps) {
-  // const [loginState, setLoginState] = useState<'idle' | 'logged-in'>('idle');
-
-  // if (loginState === 'idle') {
-  //   return (
-  //     <Login
-  //       onLogin={async (token) => {
-  //         localStorage.setItem(ACCESS_TOKEN_SESSION_KEY, token);
-  //         setLoginState('logged-in');
-  //       }}
-  //     />
-  //   );
-  // }
-
   return (
     <App>
       <Component pageProps={pageProps} />
